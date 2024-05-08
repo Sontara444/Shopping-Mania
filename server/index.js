ef22be278b227c786e9daf9deea8f9a92ae2599b
Sontara444 <sontararajput@gmail.com>
2024-05-08 18:14:19 +0530

    backend setup

---
+++ server/index.js
@@ -0,0 +1,56 @@
+const port = 4001;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+// Database connection with mongoDB
+mongoose.connect("mongodb+srv://sontaradev:000777000@cluster0.8clyxce.mongodb.net/e-commerce"
+)
+
+
+// API Creation
+
+app.get("/", (req, res)=>{
+    res.send("Express App is Running")
+})
+
+// Image Storage Engine
+
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req,file,cb)=>{
+        return cb(null,`${file.fieldname}_${Date.now()}_${path.extname(file.originalname)}`)
+    }
+})
+const upload = multer({storage:storage})
+
+//  Creating Upload Endpoint for images
+
+app.use('./images', express.static('upload/images'))
+app.post("/upload", upload.single('product'), (req, res)=>{
+    res.json({
+        success: 1,
+        image_url:`http://localhost:${port}/images`
+    })
+})
+
+
+
+app.listen(port, (error)=>{
+    if(!error){
+        console.log("Server Running on port " +port)
+    }
+    else{
+        console.log("Error : " +error)
+    }
+})
+
+
+
