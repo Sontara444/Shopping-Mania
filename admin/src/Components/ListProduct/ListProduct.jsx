3e3f026a23d08505ac20c696761eb91ebc84aea2
Sontara <sontara.dev@gamil.com>
2025-02-04 23:06:03 +0530

    complete the project

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import './ListProduct.css'
 import cross_icon from '../../assets/cross_icon.png'
 
@@ -7,7 +7,7 @@ const ListProduct = () => {
  const [allproducts, setAllProducts] = useState([]);
 
  const fetchInfo = async()=>{
-  await fetch('http://localhost:4000/allproducts')
+  await fetch('http://localhost:4001/allproducts')
   .then((res)=> res.json())
   .then((data)=>{setAllProducts(data)})
   .catch((err)=>console.log(err))
@@ -18,7 +18,7 @@ const ListProduct = () => {
  },[])
 
  const remove_product = async (id)=>{
-  await fetch('http://localhost:4000/removeproduct', {
+  await fetch('http://localhost:4001/removeproduct', {
     method:'POST',
     headers:{
       Accept:'application/json',
@@ -43,14 +43,16 @@ const ListProduct = () => {
       <div className="listproduct-allproducts">
         <hr />
         {
-          allproducts.map((product)=>{
-            return <><div key={product.id} className="listproduct-format-main listproduct-format">
-              <img src={product.image} className='listproduct-product-icon' alt="" />
+          allproducts.map((product, index)=>{
+            return <>
+            <div key={index} className="listproduct-format-main listproduct-format">
+              
+              <img src={product.image} className='listproduct-product-icon' alt="product name" />
               <p>{product.name}</p>
               <p>${product.old_price}</p>
               <p>${product.new_price}</p>
               <p>{product.category}</p>
-              <img onClick={()=>{remove_product(product.id)}} src={cross_icon} className='listproduct-remove-icon' alt="" />
+              <img onClick={()=>{remove_product(product.id)}} src={cross_icon} className='listproduct-remove-icon' alt="remove" />
             </div>
             <hr />
             </> 
